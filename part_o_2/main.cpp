c8a7cf16c1ff0671119843cedf62024d5ced708f
isaccmarting <isaccmarting@gmail.com>
2017-04-04 17:23:43 +0800

    part_o_2: text Font.

---
+++ part_o_2/main.cpp
@@ -0,0 +1,67 @@
+#include <windows.h>
+#include <GL/glut.h>
+
+// ASCII字符总共只有0到127，一共128种字符
+#define MAX_CHAR       128
+
+
+void drawString(const char* str) {
+    static int isFirstCall = 1;
+    static GLuint lists;
+
+    if( isFirstCall ) { // 如果是第一次调用，执行初始化
+                        // 为每一个ASCII字符产生一个显示列表
+        isFirstCall = 0;
+
+        // 申请MAX_CHAR个连续的显示列表编号
+        lists = glGenLists(MAX_CHAR);
+
+        // 把每个字符的绘制命令都装到对应的显示列表中
+        wglUseFontBitmaps(wglGetCurrentDC(), 0, MAX_CHAR, lists);
+    }
+    // 调用每个字符对应的显示列表，绘制每个字符
+    for(; *str!='\0'; ++str)
+        glCallList(lists + *str);
+}
+
+void selectFont(int size, int charset, const char* face) {
+    HFONT hFont = CreateFontA(size, 0, 0, 0, FW_MEDIUM, 0, 0, 0,
+        charset, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+        DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, face);
+    HFONT hOldFont = (HFONT)SelectObject(wglGetCurrentDC(), hFont);
+    DeleteObject(hOldFont);
+}
+
+void display(void) {
+    selectFont(48, ANSI_CHARSET, "Comic Sans MS");
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glRasterPos2f(0.0f, 0.0f);
+    drawString("Hello, World!");
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+
+{
+
+     glutInit(&argc, argv);
+
+     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+
+     glutInitWindowPosition(100, 100);
+
+     glutInitWindowSize(400, 400);
+
+     glutCreateWindow("OpenGL程序");
+
+     glutDisplayFunc(&display);
+
+     glutMainLoop();
+
+     return 0;
+
+}
