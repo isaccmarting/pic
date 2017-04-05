bcf2f080c32a42c5b03391ebdc8e8538cac1c53b
isaccmarting <isaccmarting@gmail.com>
2017-04-05 18:58:49 +0800

    lab2: glTranslatef(), glRotatef() and glScalef() of table.

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <GL/glut.h>
 
+// 顶点序号数组，是画六个面的顶点序号
 const GLuint index_list[] = {
 	3, 2, 1, 0, // low 
 	5, 4, 0, 1, // left 
@@ -15,17 +16,17 @@ GLfloat left_y = 0;
 GLfloat middle_rot_angle = 0.0f; 
 GLfloat right_scale = 1.0f; 
 
+// 用顶点数组画一个长方体
 // GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3, GLfloat v4, GLfloat v5, GLfloat v6, GLfloat v7 
 // (x, y) (-1,  1) (-1, -1) ( 1, -1) ( 1,  1)
-// z =  1 v4        v5      v6       v7
-// z = -1 v0        v1      v2       v3
+// z =  1 v4       v5       v6       v7
+// z = -1 v0       v1       v2       v3
 void drawCube(GLfloat vertex_list[])
 {	
-//	glFrontFace(GL_CCW); 
-//	glCullFace(GL_BACK); 
-//	glEnable(GL_CULL_FACE); 
+	// 画出长方体的边
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
 
+	// 使用顶点数组
 	glEnableClientState(GL_VERTEX_ARRAY); 
 	glVertexPointer(3, GL_FLOAT, 0, vertex_list); 
 	glDrawElements(GL_QUADS, 24, GL_UNSIGNED_INT, index_list); 
@@ -35,15 +36,20 @@ void drawCube(GLfloat vertex_list[])
 
 void reshape(int width, int height)
 {
+	// Prevent A Divide By Zero By
 	if(height == 0)
 		height = 1; 
 
+	// Reset The Current Viewport
 	glViewport(0, 0, width, height); 
 	
+	// Select The Projection Matrix
 	glMatrixMode(GL_PROJECTION); 
 	glLoadIdentity(); 
+	// Calculate The Aspect Ratio Of The Window
 	gluPerspective(75.0f, (GLfloat)width / height, 0.1f, 100.0f/*100.0f*/); 
 
+	// Select The Modelview Matrix
 	glMatrixMode(GL_MODELVIEW); 
 	glLoadIdentity(); 
 	gluLookAt(0.0f, 0.0f, 15.0f, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0f); 
@@ -51,6 +57,7 @@ void reshape(int width, int height)
 	return ; 
 }
 
+// 画出桌子
 void drawTable()
 {
 	// 桌面
@@ -76,21 +83,26 @@ void drawTable()
          0.4f,  0.4f,  0.0f,
 	}; 
 	
+	// 画出桌面
 	glPushMatrix(); 
 	drawCube(vertex_list0); 
 	glPopMatrix(); 
+	// 画出左上的桌角
 	glPushMatrix(); 
 	glTranslatef(-1.5f, 1.0f, 0.0f); 
 	drawCube(vertex_list1); 
 	glPopMatrix(); 
+	// 画出左下的桌角
 	glPushMatrix(); 
 	glTranslatef(-1.5f, -1.0f, 0.0f); 
 	drawCube(vertex_list1); 
 	glPopMatrix(); 
+	// 画出右下的桌角
 	glPushMatrix(); 
 	glTranslatef(1.5f, -1.0f, 0.0f); 
 	drawCube(vertex_list1); 
 	glPopMatrix(); 
+	// 画出右上的桌角
 	glPushMatrix(); 
 	glTranslatef(1.5f, 1.0f, 0.0f); 
 	drawCube(vertex_list1); 
@@ -103,24 +115,28 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT); 
 	glColor3f(1.0, 1.0f, 1.0f); 
 	
+	// 画出左边向上移动的桌子
 	glPushMatrix(); 
 	glTranslatef(-7.5f, 0.0f, 0.0f); 
 	glTranslatef(0.0f, left_y, 0.0f); 
 	drawTable(); 
     glPopMatrix(); 
 
+	// 画出中间旋转的桌子
 	glPushMatrix(); 
 	glRotatef(middle_rot_angle, 0.0f, 1.0f, 0.0f); 
 	drawTable(); 
     glPopMatrix(); 
 
+	// 画出右边缩小的桌子
 	glPushMatrix(); 
 	glTranslatef(7.5f, 0.0f, 0.0f); 
 	glScalef(right_scale, right_scale, right_scale); 
 	drawTable(); 
     glPopMatrix(); 
 
-    left_y += 0.002f; 
+    // 桌子的位置或大小数据改变
+	left_y += 0.002f; 
 	if(left_y >= 1.6f) left_y = 0.0f; 
 	middle_rot_angle += 0.4f; 
 	if(middle_rot_angle >= 360.0f) middle_rot_angle = 0.0f; 
