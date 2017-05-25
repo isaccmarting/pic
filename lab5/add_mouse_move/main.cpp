4f4417613854a9e0bf697578d7192f94718fde80
isaccmarting <isaccmarting@gmail.com>
2017-05-25 20:32:56 +0800

    lab5\lab6: Adding extension, vision move.

---
+++ lab5/add_mouse_move/main.cpp
@@ -0,0 +1,401 @@
+#include <stdio.h>
+#include <math.h>
+#include <GL/glut.h>
+
+// 整体已旋转的角度
+GLfloat fRotate; 
+
+// 视点坐标
+GLfloat eye[] = {0.0f, 0.0f, 8.0f}; 
+// 观察坐标
+GLfloat center[] = {0.0f, 0.0f, 0.0f}; 
+
+// light0的坐标
+GLfloat light_pos[] = {5.0f, 5.0f, 5.0f, 1.0f}; 
+// light0的颜色信息
+GLfloat *light_color; 
+
+// 聚光灯light1的坐标
+GLfloat light1_pos[] = {0.0f, 18.0f, 0.0f, 1.0f}; 
+// 聚光灯light1的朝向
+GLfloat light1_dir[] = {0.0f, -100.0f, 0.0f}; 
+// 聚光灯light1张开的角度
+GLfloat light1_angle = 1.125f; 
+
+bool bPersp = false;		// 是否透视投影
+bool bAnim = false;			// 是否旋转
+bool bWire = false;			// 是否画线
+bool bLightColor = false;	// 选择灯光的颜色
+
+GLfloat PI = 3.1415926; 
+GLfloat xViewAngle = PI/2;  // 视角与x轴的夹角 
+GLfloat yViewAngle = PI/2;  // 视角与y轴的夹角 
+GLint old_x=240, old_y=240; // 保存原先x、y的屏幕坐标 
+GLfloat r = 7.0f;			// 视野的半径 
+bool bMouseDown = false;	// 鼠标是否按下 
+
+GLint wWidth = 0, wHeight = 0; 
+
+// 画桌角
+void Draw_Leg()
+{
+	glScalef(1.0f, 1.0f, 3.0f); 
+	glutSolidCube(1.0f); 
+}
+
+// 画整个场景，茶壶和桌子
+void Draw_Triangle()
+{
+	// 茶壶的材质信息
+	GLfloat teapot_amb[] = {0.92f, 0.89f, 0.41f, 1.0f}; // 0.92f, 0.89f, 0.41f, 1.0f
+	GLfloat teapot_dif[] = {0.6f, 0.6f, 0.0f, 1.0f}; 
+	GLfloat teapot_spe[] = {0.006f, 0.006f, 0.006f, 1.0f}; 
+	// 桌面的材质信息
+	GLfloat table_face_amb[] = {1.0f, 0.0f, 0.0f, 1.0f}; 
+	GLfloat table_face_dif[] = {1.0f, 0.0f, 0.0f, 1.0f}; 
+	// 四个桌角的材质信息
+	GLfloat table_leg_amb1[] = {0.0f, 1.0f, 0.0f, 1.0f}; 
+	GLfloat table_leg_dif1[] = {0.0f, 1.0f, 0.0f, 1.0f}; 
+	GLfloat table_leg_amb2[] = {1.0f, 1.0f, 0.0f, 1.0f}; 
+	GLfloat table_leg_dif2[] = {1.0f, 1.0f, 0.0f, 1.0f}; 
+	GLfloat table_leg_amb3[] = {0.0f, 1.0f, 1.0f, 1.0f}; 
+	GLfloat table_leg_dif3[] = {0.0f, 1.0f, 1.0f, 1.0f}; 
+	GLfloat table_leg_amb4[] = {0.0f, 0.0f, 1.0f, 1.0f}; 
+	GLfloat table_leg_dif4[] = {0.0f, 0.0f, 1.0f, 1.0f}; 
+	
+	// 画茶壶
+	glFrontFace(GL_CW); 
+	// 设置材质，包括高光
+	glMaterialfv(GL_FRONT, GL_AMBIENT, teapot_amb); 
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, teapot_dif); 
+	glMaterialfv(GL_FRONT, GL_SPECULAR, teapot_spe); 
+	glMaterialf(GL_FRONT, GL_SHININESS, 3.0f); 
+	glPushMatrix(); 
+	glTranslatef(0.0f, 0.0f, 4.0f+1.0f); 
+	glRotatef(90, 1, 0, 0); 
+	glutSolidTeapot(1.0f); 
+	glPopMatrix(); 
+	glFrontFace(GL_CCW); 
+
+	// 取消高光
+	glMaterialf(GL_FRONT, GL_SHININESS, 0.0f); 
+
+	// 画桌面
+	glMaterialfv(GL_FRONT, GL_AMBIENT, table_face_amb); 
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, table_face_dif); 
+	glMaterialfv(GL_FRONT, GL_SPECULAR, table_face_dif); 
+	glPushMatrix(); 
+	glTranslatef(0.0f, 0.0f, 3.5f); 
+	glScalef(5.0f, 4.0f, 1.0f); 
+	glutSolidCube(1.0f); 
+	glPopMatrix(); 
+
+	// 画四个桌角
+	glMaterialfv(GL_FRONT, GL_AMBIENT, table_leg_amb1); 
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, table_leg_dif1); 
+	glMaterialfv(GL_FRONT, GL_SPECULAR, table_leg_dif1); 
+	glPushMatrix(); 
+	glTranslatef(1.5f, 1.0f, 1.5f); 
+	Draw_Leg(); 
+	glPopMatrix(); 
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, table_leg_amb2); 
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, table_leg_dif2); 
+	glMaterialfv(GL_FRONT, GL_SPECULAR, table_leg_dif2); 
+	glPushMatrix(); 
+	glTranslatef(-1.5f, 1.0f, 1.5f); 
+	Draw_Leg(); 
+	glPopMatrix(); 
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, table_leg_amb3); 
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, table_leg_dif3); 
+	glMaterialfv(GL_FRONT, GL_SPECULAR, table_leg_dif3); 
+	glPushMatrix(); 
+	glTranslatef(-1.5f, -1.0f, 1.5f); 
+	Draw_Leg(); 
+	glPopMatrix(); 
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, table_leg_amb4); 
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, table_leg_dif4); 
+	glMaterialfv(GL_FRONT, GL_SPECULAR, table_leg_dif4); 
+	glPushMatrix(); 
+	glTranslatef(1.5f, -1.0f, 1.5f); 
+	Draw_Leg(); 
+	glPopMatrix(); 
+}
+
+// 更新投影方式等
+void updateView(int width, int height)
+{
+	glViewport(0, 0, width, height); 
+
+	glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+	GLfloat whRatio = (GLfloat) width/height; 
+	// 透视投影还是正投影
+	if(bPersp) 
+		gluPerspective(45.0f, whRatio, 0.1f, 100.0f); 
+	else
+		glOrtho(-3, 3, -3, 3, -100, 100); 
+
+	glMatrixMode(GL_MODELVIEW); 
+}
+
+// 窗口大小改变回调函数
+void reshape(int width, int height)
+{
+	if(height == 0)
+		height = 1; 
+	wWidth = width; 
+	wHeight = height; 
+
+	updateView(wWidth, wHeight); 
+}
+
+// CPU闲置回调函数
+void idle()
+{
+	glutPostRedisplay(); 
+}
+
+// 按键回调函数
+void key(unsigned char k, int x, int y)
+{
+	switch(k)
+	{
+		case 27: // 退出
+		case 'q': {exit(0); break; }
+		// 透视投影和正投影的切换
+		case 'p': {bPersp = !bPersp; break; }
+		// 是否画线
+		case 'o': {bWire = !bWire; break; }
+		// 是否旋转
+		case ' ': {bAnim = !bAnim; break; }
+
+		case 'a': { // 相当于目标左移
+			eye[0] += 0.2f; 
+			center[0] += 0.2f; 
+			break; 
+		}
+		case 'd': { // 相当于目标右移
+			eye[0] -= 0.2f; 
+			center[0] -= 0.2f; 
+			break; 
+		}
+		case 'w': { // 相当于目标上移
+			eye[1] -= 0.2f; 
+			center[1] -= 0.2f; 
+			break; 
+		}
+		case 's': { // 相当于目标下移
+			eye[1] += 0.2f; 
+			center[1] += 0.2f; 
+			break; 
+		}
+		case 'z': { // 接近目标
+			eye[2] -= 0.2f; 
+			center[2] -= 0.2f; 
+			break; 
+		}
+		case 'c': { // 远离目标
+			eye[2] += 0.2f; 
+			center[2] += 0.2f; 
+			break; 
+		}
+
+		case 'j': { // 光源左移
+			light_pos[0] -= 0.2f; 
+			break; 
+		}
+		case 'l': { // 光源右移
+			light_pos[0] += 0.2f; 
+			break; 
+		}
+		case 'i': { // 光源上移
+			light_pos[1] += 0.2f; 
+			break; 
+		}
+		case 'k': { // 光源下移
+			light_pos[1] -= 0.2f; 
+			break; 
+		}
+		case 'n': { // 光源远离屏幕
+			light_pos[2] -= 0.2f; 
+			break; 
+		}
+		case 'm': { // 光源接近屏幕
+			light_pos[2] += 0.2f; 
+			break; 
+		}
+		case 'u': { // 切换光源颜色
+			bLightColor = !bLightColor; 
+			break; 
+		}
+
+		case 'f': { // 聚光灯朝向左移
+			light1_dir[0] -= 1.0f; 
+			break; 
+		}
+		case 'h': { // 聚光灯朝向右移
+			light1_dir[0] += 1.0f; 
+			break; 
+		}
+		case 't': { // 聚光灯朝向上移
+			light1_dir[1] += 1.0f; 
+			break; 
+		}
+		case 'g': { // 聚光灯朝向下移
+			light1_dir[1] -= 1.0f; 
+			break; 
+		}
+		case 'v': { // 聚光灯朝向远离屏幕
+			light1_dir[2] -= 1.0f; 
+			break; 
+		}
+		case 'b': { // 聚光灯朝向接近屏幕
+			light1_dir[2] += 1.0f; 
+			break; 
+		}
+		case 'r': { // 聚光灯的角度变大
+			if(light1_angle < 3.14f) light1_angle += 3.14f / 16; 
+			break; 
+		}
+		case 'y': { // 聚光灯的角度变小
+			if(light1_angle > 3.14f / 16) light1_angle -= 3.14f / 16; 
+			break; 
+		}
+	}
+	updateView(wWidth, wHeight); 
+}
+
+// 鼠标按键处理函数 
+void mouse(int button, int state, int x, int y)
+{
+	switch(button)
+	{
+		case GLUT_LEFT_BUTTON: {
+			// 鼠标释放 
+			if(state == GLUT_UP) {
+				bMouseDown = false; 
+			}
+			break; 
+		}
+		default: break; 
+	}
+}
+
+// 鼠标移动处理函数 
+void onMouseMove(int x, int y)
+{
+	GLfloat xRange = PI / 180 * 46; 
+	GLfloat yRange = PI / 180 * 46; 
+	GLfloat cos_xViewAngle, cos_yViewAngle; 
+	
+	// 第一次调用时，只记录x、y的坐标
+	if(bMouseDown == false) {
+		old_x = x; 
+		old_y = y; 
+		// 鼠标按下 
+		bMouseDown = true; 
+		return ; 
+	}
+	
+	// 更新视角与坐标轴的夹角 
+	xViewAngle = (GLfloat) (old_x - x) / wWidth * xRange + xViewAngle; 
+	yViewAngle = (GLfloat) (old_y - y) / wHeight * yRange + yViewAngle; 
+	// 限制场景在屏幕范围内 
+	if(xViewAngle < 3*PI/8) xViewAngle = 3*PI/8; 
+	else if(xViewAngle > 5*PI/8) xViewAngle = 5*PI/8; 
+	if(yViewAngle < 3*PI/8) yViewAngle = 3*PI/8; 
+	else if(yViewAngle > 5*PI/8) yViewAngle = 5*PI/8; 
+	cos_xViewAngle = cos(xViewAngle); 
+	cos_yViewAngle = cos(yViewAngle); 
+	// 更新观察点坐标
+	center[0] = eye[0] - r * cos_xViewAngle; 
+	center[1] = eye[1] + r * cos_yViewAngle; 
+	center[2] = eye[2] - r * sqrt(1 - cos_xViewAngle * cos_xViewAngle - cos_yViewAngle * cos_yViewAngle); 
+
+	// fprintf(stderr, "%lf %lf\n", xViewAngle, yViewAngle); 
+	// fprintf(stderr, "%d %d %d %d\n", old_x, x, old_y, y); 
+	old_x = x; 
+	old_y = y; 
+	// fprintf(stderr, "%lf %lf %lf\n", center[0], center[1], center[2]); 
+}
+
+// 画图回调函数
+void redraw()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	glLoadIdentity(); 
+	// 设定视点、观察坐标和观察正方向
+	gluLookAt(eye[0], eye[1], eye[2], 
+		center[0], center[1], center[2], 
+		0, 1, 0); 
+
+	if(bWire) // 是否画线
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
+	else
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); 
+
+	// 消去背面
+	glFrontFace(GL_CCW); 
+	glEnable(GL_CULL_FACE); 
+	glCullFace(GL_BACK); 
+	// 开启消影
+	glDepthFunc(GL_LESS); 
+	glEnable(GL_DEPTH_TEST); 
+	// 开启光照
+	glEnable(GL_LIGHTING); 
+	GLfloat white[] = {1.0f, 1.0f, 1.0f, 1.0f}; 
+	GLfloat blue[] = {0.0f, 0.0f, 1.0f, 1.0f}; 
+	GLfloat red[] = {1.0f, 0.0f, 0.0f, 1.0f}; 
+	GLfloat black[] = {0.0f, 0.0f, 0.0f, 1.0f}; 
+	if(bLightColor) // 切换光源颜色
+		light_color = red; 
+	else
+		light_color = white; 
+	// 设置光源light0
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos); 
+	glLightfv(GL_LIGHT0, GL_AMBIENT, black); 
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color); 
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_color); 
+	glEnable(GL_LIGHT0); 
+
+	// 设置光源light1成聚光灯
+	glLightfv(GL_LIGHT1, GL_POSITION, light1_pos); 
+	glLightfv(GL_LIGHT1, GL_AMBIENT, black); 
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, blue); 
+	glLightfv(GL_LIGHT1, GL_SPECULAR, blue); 
+	glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, light1_angle); 
+	glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, light1_dir); 
+	glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 6.0f); 
+	glEnable(GL_LIGHT1); 
+
+	// 旋转已旋转的角度，并画图
+	glRotatef(fRotate, 0, 1, 0); 
+	glRotatef(-90, 1, 0, 0); 
+	glScalef(0.2f, 0.2f, 0.2f); 
+	Draw_Triangle(); 
+
+	// 如果旋转，则已旋转的角度增加
+	if(bAnim) fRotate += 0.5f; 
+	glutSwapBuffers(); 
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv); 
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE); 
+	glutInitWindowSize(480, 480); 
+	int windowHandle = glutCreateWindow("Simple GLUT App"); 
+
+	glutDisplayFunc(redraw); 
+	glutReshapeFunc(reshape); 
+	glutKeyboardFunc(key); 
+	glutMouseFunc(mouse); 
+	glutMotionFunc(onMouseMove); 
+	glutIdleFunc(idle); 
+
+	glutMainLoop(); 
+	return 0; 
+}
---
+++ lab6/test/main.cpp
@@ -0,0 +1,452 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <windows.h>
+#include "GLee.h"
+#include <GL/glut.h>
+
+// bmp图片标志
+#define BITMAP_ID 0x4D42
+// 自定义纹理的宽和高
+#define CUSTOM_TEX_WIDTH 64
+#define CUSTOM_TEX_HEIGHT 64
+
+// 茶壶和桌子已旋转的角度
+GLfloat fRotate; 
+// 视点坐标
+GLfloat eye[] = {0.0f, 0.0f, 8.0f}; 
+// 观察坐标
+GLfloat center[] = {0.0f, 0.0f, 0.0f}; 
+
+GLfloat PI = 3.1415926; 
+// 视角和坐标轴夹角的余弦 
+GLfloat cos_xViewAngle = 0.0f, cos_yViewAngle = 0.0f; 
+// 视野的半径 
+GLfloat r = 7.0f; 
+// 临时观察点，以center为中心 
+GLfloat center_t[] = {0.0f, 0.0f, 0.0f}; 
+
+// 纹理列表
+GLuint texture[4]; 
+// 是否用茶壶的自定义纹理
+bool bTeapotCustomTex = false; 
+// 是否用桌子的混合纹理
+bool bTablePlusTex = false; 
+
+// 是否透视
+bool bPersp = false; 
+// 是否旋转
+bool bAnim = false; 
+// 是否画线
+bool bWire = false; 
+
+GLint wWidth = 0, wHeight = 0; 
+
+// 加载一个BMP图片
+// 输入文件名和保存图片信息的地址
+// 返回图片的bitmap信息
+unsigned char *LoadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader)
+{
+	FILE *filePtr; 
+	BITMAPFILEHEADER bitmapFileHeader; 
+	unsigned char *bitmapImage; 
+	int imageIdx = 0; 
+	unsigned char tempRGB; 
+
+	filePtr = fopen(filename, "rb"); 
+	if(filePtr == NULL) {printf("No %s file!\n"); exit(1); } 
+	// 读取图片文件头
+	fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, filePtr); 
+	if(bitmapFileHeader.bfType != BITMAP_ID) {
+		fprintf(stderr, "Error in LoadBitmapFile: the file is not a bitmap file\n"); 
+		exit(1); 
+	}
+	// 读取图片基本信息
+	fread(bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, filePtr); 
+	fseek(filePtr, bitmapFileHeader.bfOffBits, SEEK_SET); 
+	bitmapImage = (unsigned char*) malloc(sizeof(char) * bitmapInfoHeader -> biSizeImage); // new unsigned char[bitmapInfoHeader -> biSizeImage]; 
+	if(!bitmapImage) {
+		fprintf(stderr, "Error in LoadBitmapFile: memory error\n"); 
+		exit(1); 
+	}
+	// 读取图片bitmap
+	fread(bitmapImage, 1, bitmapInfoHeader -> biSizeImage, filePtr); 
+	if(bitmapImage == NULL) {
+		fprintf(stderr, "Error in LoadBitmapFile: memory error\n"); 
+		exit(1); 
+	}
+
+	// 交换红色和蓝色位置，BGR -> RGB
+	for(imageIdx = 0; imageIdx < bitmapInfoHeader -> biSizeImage; imageIdx += 3) {
+		tempRGB = bitmapImage[imageIdx]; 
+		bitmapImage[imageIdx] = bitmapImage[imageIdx + 2]; 
+		bitmapImage[imageIdx + 2] = tempRGB; 
+	}
+	fclose(filePtr); 
+	return bitmapImage; 
+}
+
+// 加载纹理到指定纹理位置
+// 输入纹理列表中的序号和文件名
+void TexLoad(int i, char *filename)
+{
+	BITMAPINFOHEADER bitmapInfoHeader; 
+	unsigned char *bitmapData; 
+
+	// 读取图片的bitmap
+	bitmapData = LoadBitmapFile(filename, &bitmapInfoHeader); 
+	// 载入图片纹理
+	glBindTexture(GL_TEXTURE_2D, texture[i]); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); 
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 
+		bitmapInfoHeader.biWidth, bitmapInfoHeader.biHeight, 0, 
+		GL_RGB, GL_UNSIGNED_BYTE, bitmapData); 
+}
+
+// 初始化纹理
+void InitTex()
+{
+	unsigned char customImage[CUSTOM_TEX_HEIGHT][CUSTOM_TEX_WIDTH][3]; 
+	int i, j; 
+	char *teapotPic, *tablePic, *tablePic_Plus; 
+	teapotPic = (char*) "Monet.bmp"; 
+	tablePic = (char*) "Crack.bmp"; 
+	tablePic_Plus = (char*) "Spot.bmp"; 
+	// 生成纹理列表
+	glGenTextures(4, texture); 
+	// 分别加载这些图片的纹理
+	TexLoad(0, teapotPic); 
+	TexLoad(1, tablePic); 
+	TexLoad(3, tablePic_Plus); 
+
+	// 生成自定义纹理
+	for(i = 0; i < CUSTOM_TEX_HEIGHT; i++)
+		for(j = 0; j < CUSTOM_TEX_WIDTH; j++)
+		{
+			// 根据位置计算这个位置是红色还是黑色
+			customImage[i][j][0] = (!((i & 4) ^ (j & 4))) * 255; 
+			customImage[i][j][1] = 0; 
+			customImage[i][j][2] = 0; 
+		}
+	glBindTexture(GL_TEXTURE_2D, texture[2]); 
+	// 定义存储的对齐方式
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); 
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 
+		CUSTOM_TEX_WIDTH, CUSTOM_TEX_HEIGHT, 0, 
+		GL_RGB, GL_UNSIGNED_BYTE, customImage); 
+}
+
+// 画一个有纹理的立方体
+void Draw_Tex_Cube(GLfloat ratio)
+{
+	GLfloat x, y, z; 
+	x=y=z = ratio/2; 
+	// 定义立方体的每个点坐标
+	// up:up, dn:down; fw:forward, bk:back; lf:left, rt:right; 
+	GLfloat A[] = {+x, -y, -z}, // DnFwRt 
+			B[] = {-x, -y, -z}, // DnFwLf 
+			C[] = {-x, +y, -z}, // DnBkLf 
+			D[] = {+x, +y, -z}, // DnBkRt 
+			E[] = {+x, -y, +z}, // UpFwRt 
+			F[] = {-x, -y, +z}, // UpFwLf 
+			G[] = {-x, +y, +z}, // UpBkLf 
+			H[] = {+x, +y, +z}; // UpBkRt 
+	// 定义立方体的每个面
+	GLfloat *vertices[][4] = {
+		{A, B, C, D}, 
+		{C, B, F, G}, 
+		{B, A, E, F}, 
+		{A, D, H, E}, 
+		{D, C, G, H}, 
+		{F, E, H, G} 
+	}; 
+	GLfloat texCoord[][2] = {
+		{0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 1.0f}
+	}; 
+	int i, j; 
+
+	// 画出带有纹理的立方体
+	glBegin(GL_QUADS); 
+	for(i = 0; i < 6; i++) 
+		for(j = 0; j < 4; j++)
+		{
+			if(!bTablePlusTex) { // 只有一层纹理
+				glTexCoord2fv(texCoord[j]); 
+			}
+			else { // 有两层纹理
+				glMultiTexCoord2fvARB(GL_TEXTURE0_ARB, texCoord[j]); 
+				glMultiTexCoord2fvARB(GL_TEXTURE1_ARB, texCoord[j]); 
+			}
+			glVertex3fv(vertices[i][j]); 
+		}
+	glEnd(); 
+}
+
+// 画出有纹理的桌腿
+void Draw_Tex_Leg()
+{
+	glScalef(1.0f, 1.0f, 3.0f); 
+	// glutSolidCube(1.0f); 
+	Draw_Tex_Cube(1.0f); 
+}
+
+// 画出整个场景
+void Draw_Triangle()
+{
+	// 茶壶的材质信息
+	GLfloat teapot_amb[] = {0.3f, 0.3f, 0.0f, 1.0f}; 
+	GLfloat teapot_dif[] = {0.5f, 0.5f, 0.5f, 1.0f}; 
+	GLfloat teapot_spe[] = {1.0f, 1.0f, 1.0f, 1.0f}; 
+	
+	// 画茶壶
+	// 设置茶壶的材质
+	glMaterialfv(GL_FRONT, GL_AMBIENT, teapot_amb); 
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, teapot_dif); 
+	glMaterialfv(GL_FRONT, GL_SPECULAR, teapot_spe); 
+	glMaterialf(GL_FRONT, GL_SHININESS, 60.0f); 
+	glPushMatrix(); 
+	glTranslatef(0.0f, 0.0f, 4.0f+1.0f); 
+	glRotatef(90, 1, 0, 0); 
+	// 激活并开启纹理
+	glActiveTextureARB(GL_TEXTURE0_ARB); 
+	glEnable(GL_TEXTURE_2D); 
+	if(!bTeapotCustomTex) // 用图片纹理
+		glBindTexture(GL_TEXTURE_2D, texture[0]); 
+	else // 用自定义纹理
+		glBindTexture(GL_TEXTURE_2D, texture[2]); 
+	// 纹理和光照叠加
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE); 
+	GLfloat white[] = {1.0f, 1.0f, 1.0f, 1.0f}; 
+	glTexEnvfv(GL_TEXTURE_ENV, GL_TEXTURE_ENV_COLOR, white); 
+	glutSolidTeapot(1.0f); 
+	glDisable(GL_TEXTURE_2D); 
+	glPopMatrix(); 
+
+	// 画桌子
+	// 设置第一层纹理，绑定桌子纹理图片
+	glEnable(GL_TEXTURE_2D); 
+	glBindTexture(GL_TEXTURE_2D, texture[1]); 
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL); 
+	if(bTablePlusTex) { // 设置第二层纹理，绑定另一张图片
+		glActiveTextureARB(GL_TEXTURE1_ARB); 
+		glEnable(GL_TEXTURE_2D); 
+		glBindTexture(GL_TEXTURE_2D, texture[3]); 
+		// 使两张纹理可叠加
+		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE); 
+	}
+	
+	// 画桌面
+	glPushMatrix(); 
+	glTranslatef(0.0f, 0.0f, 3.5f); 
+	glScalef(5.0f, 4.0f, 1.0f); 
+	// glutSolidCube(1.0f); 
+	Draw_Tex_Cube(1.0f); 
+	glPopMatrix(); 
+
+	// 画桌角
+	glPushMatrix(); 
+	glTranslatef(1.5f, 1.0f, 1.5f); 
+	Draw_Tex_Leg(); 
+	glPopMatrix(); 
+
+	glPushMatrix(); 
+	glTranslatef(-1.5f, 1.0f, 1.5f); 
+	Draw_Tex_Leg(); 
+	glPopMatrix(); 
+
+	glPushMatrix(); 
+	glTranslatef(-1.5f, -1.0f, 1.5f); 
+	Draw_Tex_Leg(); 
+	glPopMatrix(); 
+
+	glPushMatrix(); 
+	glTranslatef(1.5f, -1.0f, 1.5f); 
+	Draw_Tex_Leg(); 
+	glPopMatrix(); 
+
+	// 关闭每层纹理
+	glActiveTextureARB(GL_TEXTURE0_ARB); 
+	glDisable(GL_TEXTURE_2D); 
+	glActiveTextureARB(GL_TEXTURE1_ARB); 
+	glDisable(GL_TEXTURE_2D); 
+}
+
+// 更新视图和投影
+void updateView(int width, int height)
+{
+	glViewport(0, 0, width, height); 
+
+	glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+	GLfloat whRatio = (GLfloat) width / height; 
+	if(bPersp)
+		gluPerspective(45.0f, whRatio, 0.1f, 100.0f); 
+	else
+		glOrtho(-3, 3, -3, 3, -100, 100); 
+
+	glMatrixMode(GL_MODELVIEW); 
+}
+
+// 窗口形状改变回调函数
+void reshape(int width, int height)
+{
+	if(height == 0)
+		height = 1; 
+
+	wWidth = width; 
+	wHeight = height; 
+
+	updateView(width, height); 
+}
+
+// CPU闲置回调函数
+void idle()
+{
+	glutPostRedisplay(); 
+}
+
+// 键盘回调函数
+void key(unsigned char k, int x, int y)
+{
+	switch(k)
+	{
+		case 27: // 退出
+		case 'q': {exit(0); break; }
+		// 是否旋转
+		case ' ': {bAnim = !bAnim; break; }
+		// 是否画线
+		case 'o': {bWire = !bWire; break; }
+		// 是否透视投影
+		case 'p': {bPersp = !bPersp; break; }
+
+		case 'a': { // 物体左移
+			eye[0] += 0.2f; 
+			center[0] += 0.2f; 
+			break; 
+		}
+		case 'd': { // 物体右移
+			eye[0] -= 0.2f; 
+			center[0] -= 0.2f; 
+			break; 
+		}
+		case 'w': { // 物体上移
+			eye[1] -= 0.2f; 
+			center[1] -= 0.2f; 
+			break; 
+		}
+		case 's': { // 物体下移
+			eye[1] += 0.2f; 
+			center[1] += 0.2f; 
+			break; 
+		}
+		case 'z': { // 接近物体
+			eye[2] -= 0.2f; 
+			center[2] -= 0.2f; 
+			break; 
+		}
+		case 'c': { // 远离物体
+			eye[2] += 0.2f; 
+			center[2] += 0.2f; 
+			break; 
+		}
+
+		case 'i': { // 切换茶壶的纹理
+			bTeapotCustomTex = !bTeapotCustomTex; 
+			break; 
+		}
+		case 'k': { // 切换桌子的纹理
+			bTablePlusTex = !bTablePlusTex; 
+			break; 
+		}
+	}
+	updateView(wWidth, wHeight); 
+}
+
+// 鼠标移动处理函数 
+void onMouseMove(int x, int y)
+{
+	// fprintf(stderr, "x: %d; y: %d\n", x, y); 
+	GLfloat xRange = PI / 180 * 30; 
+	GLfloat yRange = PI / 180 * 23; 
+	GLfloat xViewAngle; 
+	GLfloat yViewAngle; 
+	// 更新当前的视角与坐标轴的夹角 
+	xViewAngle = (2.0f * x / wWidth - 1) * xRange + PI/2; 
+	yViewAngle = (2.0f * y / wHeight - 1) * yRange + PI/2; 
+	// 保存余弦值 
+	cos_xViewAngle = cos(xViewAngle); 
+	cos_yViewAngle = cos(yViewAngle); 
+	// fprintf(stderr, "%lf %lf\n", 2.0f * x / wWidth - 1, 2.0f * y / wHeight - 1); 
+	// fprintf(stderr, "%lf %lf %lf\n", center_t[0], center_t[1], center_t[2]); 
+}
+
+// 画图回调函数
+void redraw()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	glLoadIdentity(); 
+
+	// 计算的临时观察点坐标 
+	center_t[0] = eye[0] - r * cos_xViewAngle; 
+	center_t[1] = eye[1] + r * cos_yViewAngle; 
+	center_t[2] = eye[2] - r * sqrt(1 - cos_xViewAngle * cos_xViewAngle - cos_yViewAngle * cos_yViewAngle); 
+	// 视点、观察坐标和观察正方向
+	gluLookAt(eye[0], eye[1], eye[2], 
+		center_t[0], center_t[1], center_t[2], 
+		0, 1, 0); 
+
+	if(bWire) // 画线
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
+	else      // 画面
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); 
+
+	// 开启消影
+	glDepthFunc(GL_LESS); 
+	glEnable(GL_DEPTH_TEST); 
+	// 开启光源
+	glEnable(GL_LIGHTING); 
+	GLfloat white[] = {1.0f, 1.0f, 1.0f, 1.0f}; 
+	GLfloat black[] = {0.0f, 0.0f, 0.0f, 1.0f}; 
+	GLfloat light_pos[] = {5.0f, 5.0f, 5.0f, 1.0f}; 
+
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos); 
+	glLightfv(GL_LIGHT0, GL_AMBIENT, black); 
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, white); 
+	glLightfv(GL_LIGHT0, GL_SPECULAR, white); 
+	glEnable(GL_LIGHT0); 
+
+	// 画出场景
+	glRotatef(fRotate, 0, 1, 0); 
+	glRotatef(-90, 1, 0, 0); 
+	glScalef(0.2f, 0.2f, 0.2f); 
+	Draw_Triangle(); 
+
+	// 是否旋转，以及改变已旋转角度
+	if(bAnim) fRotate += 0.5f; 
+	glutSwapBuffers(); 
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv); 
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE); 
+	glutInitWindowSize(480, 480); 
+	int windowHandler = glutCreateWindow("Simple GLUT App"); 
+
+	glutDisplayFunc(redraw); 
+	glutIdleFunc(idle); 
+	glutReshapeFunc(reshape); 
+	glutKeyboardFunc(key); 
+
+	// 鼠标没有按下时移动处理函数 
+	glutPassiveMotionFunc(onMouseMove); 
+
+	InitTex(); 
+	glutMainLoop(); 
+	return 0; 
+}
